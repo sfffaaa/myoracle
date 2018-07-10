403ccbdedd135e9a5440a785b6f347cbac3980c2
Jay Pan <sfffaaa@gmail.com>
2018-07-10 20:35:00 +0200

    Add new storage type

--- solidity/contracts/OracleStorage.sol
+++ solidity/contracts/OracleStorage.sol
@@ -2,6 +2,7 @@ pragma solidity 0.4.24;
 
 contract OracleStorage {
     mapping(bytes32 => mapping(bytes32 => address)) private bytes32ToAddress;
+    mapping(bytes32 => address[]) private addressArray;
 
     function delBytes32ToAddress(string _name, bytes32 _key)
         public
@@ -24,5 +25,65 @@ contract OracleStorage {
     {
         bytes32 name = keccak256(abi.encodePacked(_name));
         return bytes32ToAddress[name][_key];
+    }
+
+
+
+    function getAddressArrayLength(string _name)
+        view
+        public
+        returns(uint)
+    {
+        bytes32 name = keccak256(abi.encodePacked(_name));
+        return addressArray[name].length;
+    }
+
+    function getAddressArrayEntry(string _name, uint _idx)
+        view
+        public
+        returns(address)
+    {
+        require(getAddressArrayLength(_name) > _idx);
+
+        bytes32 name = keccak256(abi.encodePacked(_name));
+        return addressArray[name][_idx];
+    }
+
+    function setAddressArrayEntry(string _name, uint _idx, address _val)
+        public
+    {
+        require(getAddressArrayLength(_name) > _idx);
+        bytes32 name = keccak256(abi.encodePacked(_name));
+        addressArray[name][_idx] = _val;
+    }
+
+    function pushAddressArrayEntry(string _name, address _val)
+        public
+    {
+        bytes32 name = keccak256(abi.encodePacked(_name));
+        addressArray[name].push(_val);
+    }
+
+    function delAddressArrayEntry(string _name, uint _idx)
+        public
+    {
+        require(getAddressArrayLength(_name) > _idx);
+
+        bytes32 name = keccak256(abi.encodePacked(_name));
+        delete addressArray[name][_idx];
+    }
+
+    function delAddressArray(string _name)
+        public
+    {
+        bytes32 name = keccak256(abi.encodePacked(_name));
+        delete addressArray[name];
+    }
+
+    function changeAddressArrayLength(string _name, uint _length)
+        public
+    {
+        bytes32 name = keccak256(abi.encodePacked(_name));
+        addressArray[name].length = _length;
     }
 }
--- solidity/test/OracleStorage.js
+++ solidity/test/OracleStorage.js
@@ -36,4 +36,48 @@ contract('OracleStorageBasic', () => {
         const myAddress = await oracleStorageInst.getBytes32ToAddress('NON_EXIST_2', 'key');
         assert.equal(myAddress, 0, 'Two address should be the same');
     });
+
+    it('Action on addressArray checking', async () => {
+        const ARRAY_TEST_KEYS = 'AddressArrayChecking';
+        assert.equal(await oracleStorageInst.getAddressArrayLength(ARRAY_TEST_KEYS), 0,
+            'There is no any array entry');
+
+        oracleStorageInst.pushAddressArrayEntry(ARRAY_TEST_KEYS, 0x01);
+        assert.equal(await oracleStorageInst.getAddressArrayLength(ARRAY_TEST_KEYS), 1,
+            'Length should be the same');
+        assert.equal(await oracleStorageInst.getAddressArrayEntry(ARRAY_TEST_KEYS, 0), 0x01,
+            'Entry should be the same');
+
+        await oracleStorageInst.setAddressArrayEntry(ARRAY_TEST_KEYS, 0, 0x02);
+        assert.equal(await oracleStorageInst.getAddressArrayLength(ARRAY_TEST_KEYS), 1,
+            'Length should be the same');
+        assert.equal(await oracleStorageInst.getAddressArrayEntry(ARRAY_TEST_KEYS, 0), 0x02,
+            'Entry should be the same');
+
+        await oracleStorageInst.pushAddressArrayEntry(ARRAY_TEST_KEYS, 0x03);
+        assert.equal(await oracleStorageInst.getAddressArrayLength(ARRAY_TEST_KEYS), 2,
+            'Length should be the same');
+        assert.equal(await oracleStorageInst.getAddressArrayEntry(ARRAY_TEST_KEYS, 0), 0x02,
+            'Entry should be the same');
+        assert.equal(await oracleStorageInst.getAddressArrayEntry(ARRAY_TEST_KEYS, 1), 0x03,
+            'Entry should be the same');
+
+        await oracleStorageInst.delAddressArrayEntry(ARRAY_TEST_KEYS, 0);
+        assert.equal(await oracleStorageInst.getAddressArrayLength(ARRAY_TEST_KEYS), 2,
+            'Length should be the same');
+        assert.equal(await oracleStorageInst.getAddressArrayEntry(ARRAY_TEST_KEYS, 0), 0,
+            'Entry should be the same');
+        assert.equal(await oracleStorageInst.getAddressArrayEntry(ARRAY_TEST_KEYS, 1), 0x03,
+            'Entry should be the same');
+
+        await oracleStorageInst.changeAddressArrayLength(ARRAY_TEST_KEYS, 1);
+        assert.equal(await oracleStorageInst.getAddressArrayLength(ARRAY_TEST_KEYS), 1,
+            'Length should be the same');
+        assert.equal(await oracleStorageInst.getAddressArrayEntry(ARRAY_TEST_KEYS, 0), 0,
+            'Entry should be the same');
+
+        await oracleStorageInst.delAddressArray(ARRAY_TEST_KEYS);
+        assert.equal(await oracleStorageInst.getAddressArrayLength(ARRAY_TEST_KEYS), 0,
+            'Length should be the same');
+    });
 });
