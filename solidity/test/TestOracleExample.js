5d0909b396cff8c8d4e1139da8eff5b5543d36f1
Jay Pan <sfffaaa@gmail.com>
2018-07-11 07:57:41 +0200

    Test callback function with queryId

--- solidity/test/TestOracleExample.js
+++ solidity/test/TestOracleExample.js
@@ -1,4 +1,5 @@
-/* global artifacts, contract, it, assert */
+/* global artifacts, contract, it, assert, web3 */
+const truffleAssert = require('truffle-assertions');
 const TestUtils = require('./TestUtils.js');
 
 const OracleCore = artifacts.require('OracleCore');
@@ -12,11 +13,30 @@ contract('TestOracleExample', () => {
         await testOracleExampleInst.querySentNode('fake request');
         let oracleData = {};
         const toOracleNodeEvent = oracleCoreInst.ToOracleNode({}, { fromBlock: 0, toBlock: 'latest' });
-        const oracleLogs = await TestUtils.WaitContractEventGet(toOracleNodeEvent);
+        let oracleLogs = await TestUtils.WaitContractEventGet(toOracleNodeEvent);
         oracleData = oracleLogs[oracleLogs.length - 1].args;
         assert.equal(oracleData.requests, 'fake request', 'Request should be the same');
 
         const queryId = await testOracleExampleInst.getLastestQueryId();
         assert.equal(oracleData.queryId, queryId, 'QueryId should be the same');
+
+        // Simulate respone without running node
+        const tx = await oracleCoreInst.resultSentBack(queryId, 'fake response', web3.sha3('fake response'));
+        truffleAssert.eventEmitted(tx, 'ToOracleCallee', (ev) => {
+            return ev.queryId === queryId && ev.callee === TestOracleExample.address;
+        });
+
+        const toOracleCalleeEvent = oracleCoreInst.ToOracleCallee({}, { fromBlock: 0, toBlock: 'latest' });
+        oracleLogs = await TestUtils.WaitContractEventGet(toOracleCalleeEvent);
+        oracleData = oracleLogs[oracleLogs.length - 1].args;
+
+        let calleeData = {};
+        const showCallbackEvent = testOracleExampleInst.ShowCallback({}, { fromBlock: 0, toBlock: 'latest' });
+        const calleeLogs = await TestUtils.WaitContractEventGet(showCallbackEvent);
+        calleeData = calleeLogs[calleeLogs.length - 1].args;
+        calleeData.callee = testOracleExampleInst.address;
+        if (TestUtils.CheckObjectEqual(oracleData, calleeData) === false) {
+            assert.equal(oracleData, calleeData, 'Two object should be the same');
+        }
     });
 });
