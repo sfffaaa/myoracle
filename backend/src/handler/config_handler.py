8e975e1bf5a90d76b8b8efa718d8b2094abf1922
Jay Pan <sfffaaa@gmail.com>
2018-08-16 16:15:34 +0200

    Fix no config error

--- backend/src/handler/config_handler.py
+++ backend/src/handler/config_handler.py
@@ -10,12 +10,17 @@ from web3 import Web3
 class ConfigHandler():
     def __init__(self, config_path=CONFIG_PATH):
         self._config_path = config_path
+        self._config = configparser.ConfigParser()
+        self._config.read(self._config_path)
 
     def get_chain_config(self, section, key):
-        config = configparser.ConfigParser()
-        config.read(self._config_path)
-        return config.get(section, key)
+        return self._config.get(section, key)
 
     def get_web3(self):
-        file_ipc = self.get_chain_config('Ethereum', 'file_ipc')
-        return Web3(Web3.IPCProvider(file_ipc))
+        keys = ['file_ipc', 'socket_ipc']
+        ipc_configs = [self.get_chain_config('Ethereum', key) for key in keys
+                       if self._config.has_option('Ethereum', key)]
+        ipc_configs = [ipc_config for ipc_config in ipc_configs if ipc_config]
+        if not len(ipc_configs):
+            raise IOError("keys[{0}] doesn't have data".format(','.join(keys)))
+        return Web3(Web3.IPCProvider(ipc_configs[0]))
