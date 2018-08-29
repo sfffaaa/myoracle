236f3addfc86fbd47c2e3b55f4bd64c08c3030c6
Jay Pan <sfffaaa@gmail.com>
2018-08-29 17:30:14 +0200

    Add private chain related file in travis CI

---
+++ backend/travisCI/startnode1.sh
@@ -0,0 +1,2 @@
+geth --datadir "datadir" --keystore "keystore" init genesis.json
+geth --identity "node1" --networkid 1994 --datadir "datadir" --keystore "keystore" --nodiscover --rpc --port "30303" --unlock "0,1,2,3,4" --password "password.sec"
