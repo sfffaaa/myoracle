2f2f0295f7fcff5f6e3cf7bfded293adb3bb7f76
Jay Pan <sfffaaa@gmail.com>
2018-08-29 18:38:55 +0200

    Show geth log for checking

--- backend/travisCI/test.sh
+++ backend/travisCI/test.sh
@@ -4,7 +4,7 @@ set -e
 cd backend
 
 # start private chain
-(cd travisCI; sh startnode1.sh 2> /dev/null 1> /dev/null &)
+(cd travisCI; sh startnode1.sh &)
 sleep 10
 
 python3 testcase/oracle_wallet_test.py
