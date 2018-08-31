afd4c39aa91a28ea86b32f4ea0c78d3c3c804498
Jay Pan <sfffaaa@gmail.com>
2018-08-31 12:19:18 +0200

    Add cache in travis for speedup

--- backend/travisCI/startnode1.sh
+++ backend/travisCI/startnode1.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+set -e
+
 geth --datadir "datadir" --keystore "keystore" init genesis.json
 geth --identity "node1" --networkid 1994 --datadir "datadir" --keystore "keystore" --nodiscover --rpc --port "30303" --unlock "0,1,2,3,4" --password "password.sec" --mine --minerthreads=1
--- backend/travisCI/test.sh
+++ backend/travisCI/test.sh
@@ -3,8 +3,19 @@
 set -e
 cd backend
 
+ETHASH_PATH_EXIST=1
+
+if [ ! -d "${HOME}/.ethash" ]; then
+	ETHASH_PATH_EXIST=0
+fi
+
 # start private chain
 (cd travisCI; sh startnode1.sh &)
-sleep 240
 
+if [ x"${ETHASH_PATH_EXIST}" == x"0" ]; then
+	echo "==== Wait for Ethash finish first time ===="
+	sleep 240
+fi
+
+echo "==== Running test case ===="
 sh testcase/test_run.sh
