14ed10d6bb10a5a07adceb80b52967819744041c
Jay Pan <sfffaaa@gmail.com>
2018-08-31 12:47:27 +0200

    Partial revert 0c00c1c and afd4c39, need to check how to speedup again

--- backend/travisCI/test.sh
+++ backend/travisCI/test.sh
@@ -3,23 +3,11 @@
 set -e
 cd backend
 
-ETHASH_PATH_EXIST=1
-
-if [ ! -d "${HOME}/.ethash" ]; then
-	ETHASH_PATH_EXIST=0
-fi
-
-if [ ! -d "travisCI/datadir/geth/ethash" ]; then
-	ETHASH_PATH_EXIST=0
-fi
-
 # start private chain
 (cd travisCI; sh startnode1.sh &)
 
-if [ x"${ETHASH_PATH_EXIST}" == x"0" ]; then
-	echo "==== Wait for Ethash finish first time ===="
-	sleep 240
-fi
+echo "==== Wait for Ethash finish first time ===="
+sleep 240
 
 echo "==== Running test case ===="
 sh testcase/test_run.sh
